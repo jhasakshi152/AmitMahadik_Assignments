c7c591b80403463a92af7d5f67e2f6d964c5cd17
jhasakshi152 <62647612+jhasakshi152@users.noreply.github.com>
2022-02-26 16:23:45 +0530

    Update Assignment1Q9.java

--- Core Java/Basic data structure Assignment/Assignment1Q9.java
+++ Core Java/Basic data structure Assignment/Assignment1Q9.java
@@ -48,20 +48,6 @@ public class Assignment1Q9 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		int []m1 = new int[3];
-		int []m2 = new int[3];
-		int []m3 = new int[3];
-		for(int i = 0; i < 3; i++) {
-			System.out.println("Enter marks for subjects A, B, C : ");
-            m1[i] = sc.nextInt();
-            m2[i] = sc.nextInt();
-            m3[i] = sc.nextInt();
-		}
-		Student s = new Student();
-		System.out.print("Students total marks in all subjects: ");
-		s.studentsTotalMarksInAllSubjects();
-		
 		
 
 	}
